fe472841a49c67ab147e949120e021b94af04947
parker lindley <fugazi8@gmail.com>
2017-12-16 18:08:53 -0700

    Initial commit

--- scripts/LinkedList.js
+++ scripts/LinkedList.js
@@ -1,7 +1,80 @@
 import Node from './Node';
 
 export default class LinkedList {
-  constructor () {
-
+  constructor (length, head) {
+    this.length = 0;
+    this.head = null;
   }
+
+  unshift(item) {
+    let newNode = new Node(item);
+    let temp = this.head
+
+    if (this.head === null) {
+      this.head = newNode;
+      this.length++;
+
+      return newNode;
+    }
+
+    this.head = newNode;
+    this.head.next = temp;
+    this.length++;
+    return newNode;
+  }
+
+  shift() {
+    if (this.length === 0) {
+      return null;
+    } else {
+      let temp = this.head;
+      this.head = this.head.next;
+      return temp;
+    }
+  }
+
+  push(item) {
+    let newNode = new Node(item);
+    let currentNode = this.head;
+
+    if (this.head === null) {
+      this.head = newNode;
+      this.length++
+      return newNode;
+    }
+
+    while(currentNode.next !== null) {
+      currentNode = currentNode.next;
+    }
+
+    currentNode.next = newNode;
+    this.length++
+  }
+
+  pop() {
+    let currentNode = this.head;
+    let previous = null;
+    
+    if (this.length === 0) {
+      return null;
+    }
+
+    if (this.length === 1) {
+      let last = this.head;
+      this.head = null;
+      this.length--
+      return last;
+    }
+
+    while(currentNode.next !== null) {
+      previous = currentNode;
+      currentNode = currentNode.next;
+    }
+
+    previous.next = null;
+
+    this.length--;
+    return currentNode;
+  }
+
 }
